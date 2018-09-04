08a0cff1db3d7ddf8a1d2c9de64dc1e371d1b986
Oualid <Oualid.elkhalki2001@gmail.com>
2018-09-04 11:05:53 +0200

    Je kan een promt toevoegen etc

---
+++ lab1.js
@@ -0,0 +1,13 @@
+alert ('U dient 3 vragen te beantwoorden.');
+
+document.write("voer hier je voornaam in: ");
+document.write(prompt( "voer hier je voornaam in.")); 
+document.write("<br>");
+
+document.write("voer hier je achternaam in: "); 
+document.write(prompt( "voer hier je achternaam in."));
+document.write("<br>");
+
+document.write("voer hier je leeftijd in: ");
+document.write(prompt( "voer hier je leeftijd in."));
+document.write("<br>");
