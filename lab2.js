549009adc5bacc5d9fbd5ea8c2a2da81f096c41f
Oualid <Oualid.elkhalki2001@gmail.com>
2018-09-04 12:57:54 +0200

    lab 2

--- lab2.js
+++ lab2.js
@@ -1,10 +1,18 @@
-<!-- /* Oualid El Khalki 16 jaar 
-	aplicatie ontwikkelaar niveau 4*/ -->
+/*
+Oualid El Khalki 
+
+*/
+var a =prompt("geef waarde voor a");
+
+const ALERT = alert('U dient 3 vragen te beantwoorden.'); //geeft een alert aan
 
 
-alert ('U dient 3 vragen te beantwoorden.'); //geeft een bericht wanneer je naar een site gaat
 
-document.write("voer hier je voornaam in: "); //moet je beantwoorden
+
+
+
+
+document.write("voer hier je voornaam in: ");
 document.write(prompt( "voer hier je voornaam in.")); 
 document.write("<br>");
 
@@ -15,3 +23,6 @@ document.write("<br>");
 document.write("voer hier je leeftijd in: ");
 document.write(prompt( "voer hier je leeftijd in."));
 document.write("<br>");
+
+
+
