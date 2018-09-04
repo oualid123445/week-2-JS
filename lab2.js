67be907ead56558a582870491739fb616ce7d1b1
Oualid <Oualid.elkhalki2001@gmail.com>
2018-09-04 14:03:08 +0200

    klaar

--- lab2.js
+++ lab2.js
@@ -1,5 +1,6 @@
 /*
 Oualid El Khalki 
+ Aplicatie en media ontwikkelaar.
 
 */
 var a =prompt("geef waarde voor a");
@@ -9,20 +10,15 @@ const ALERT = alert('U dient 3 vragen te beantwoorden.'); //geeft een alert aan
 
 
 
+var voornaam = prompt("geef je voornaam.")
+var achternaam = prompt("geef je achternaam.")
+var leeftijd = prompt("geef je leeftijd.")
 
 
 
-document.write("voer hier je voornaam in: ");
-document.write(prompt( "voer hier je voornaam in.")); 
-document.write("<br>");
 
-document.write("voer hier je achternaam in: "); 
-document.write(prompt( "voer hier je achternaam in."));
-document.write("<br>");
-
-document.write("voer hier je leeftijd in: ");
-document.write(prompt( "voer hier je leeftijd in."));
-document.write("<br>");
-
+document.write("<br>je voornaam: "+ voornaam);
+document.write("<br>je achternaam: " + achternaam);
+document.write("<br>je leeftijd: " + leeftijd);
 
 
