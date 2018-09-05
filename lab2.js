37b96ca84798415aa3882516d226a88d3710f807
Oualid <Oualid.elkhalki2001@gmail.com>
2018-09-05 11:36:14 +0200

    Lab 3

--- lab2.js
+++ lab2.js
@@ -5,20 +5,20 @@ Oualid El Khalki
 */
 var a =prompt("geef waarde voor a");
 
-const ALERT = alert('U dient 3 vragen te beantwoorden.'); //geeft een alert aan
+const ALERT = alert('U dient 3 vragen te beantwoorden.'); //maakt een constante aan met de melding
 
 
 
 
-var voornaam = prompt("geef je voornaam.")
-var achternaam = prompt("geef je achternaam.")
-var leeftijd = prompt("geef je leeftijd.")
+var voornaam = prompt("geef je voornaam.") // declaratie variabele voornaam
+var achternaam = prompt("geef je achternaam.") // declaratie variabele achternaam
+var leeftijd = prompt("geef je leeftijd.") // declaratie variabele leeftijd 
 
 
 
 
-document.write("<br>je voornaam: "+ voornaam);
-document.write("<br>je achternaam: " + achternaam);
-document.write("<br>je leeftijd: " + leeftijd);
+document.write("<br>je voornaam: "+ voornaam); //toont voornaam op scherm
+document.write("<br>je achternaam: " + achternaam); // toont achternaam op scherm
+document.write("<br>je leeftijd: " + leeftijd); // toont leeftijd op scherm
 
 
---
+++ lab3.js
@@ -0,0 +1,56 @@
+/*
+Oualid El Khalki 
+ Aplicatie en media ontwikkelaar.
+
+*/
+var GETAL = prompt('vul cijfer in')
+var getal = 12
+var invoer;
+// de tafel van GETAL
+document.write('<br>1 x '+ GETAL +'=' + GETAL);
+document.write('<br>2 x '+ GETAL +'=' + GETAL);
+document.write('<br>3 x '+ GETAL +'=' + GETAL);
+document.write('<br>4 x '+ GETAL +'=' + GETAL);
+document.write('<br>5 x '+ GETAL +'=' + GETAL);
+document.write('<br>6 x '+ GETAL +'=' + GETAL); 
+document.write('<br>7 x '+ GETAL +'=' + GETAL);
+document.write('<br>8 x '+ GETAL +'=' + GETAL);
+document.write('<br>9 x '+ GETAL +'=' + GETAL); 
+document.write('<br>10 x '+ GETAL +'=' + GETAL);
+
+
+
+document.write('<br><br><br>'+ getal +'+ 6=');
+getal = getal + 6;
+document.write(getal);
+
+
+document.write('<br>' + getal + ' * 10 = ');
+getal= getal * 10;
+document.write(getal);
+
+
+document.write('<br>' + getal + ' / 5 = ');
+getal= getal / 5;
+document.write(getal);
+
+
+document.write('<br>' + getal + ' - 12 = ');
+getal= getal - 12;
+document.write(getal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
