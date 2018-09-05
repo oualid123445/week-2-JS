33ea03b7b33890110de30ba1f29f06a3c030d2a9
Oualid <Oualid.elkhalki2001@gmail.com>
2018-09-05 11:50:10 +0200

    volledig lab 3

--- lab3.js
+++ lab3.js
@@ -3,20 +3,21 @@ Oualid El Khalki
  Aplicatie en media ontwikkelaar.
 
 */
-var GETAL = prompt('vul cijfer in')
+var GETAL = prompt('vul cijfer in',GETAL);
+GETAL = parseInt(GETAL)
 var getal = 12
 var invoer;
 // de tafel van GETAL
-document.write('<br>1 x '+ GETAL +'=' + GETAL);
-document.write('<br>2 x '+ GETAL +'=' + GETAL);
-document.write('<br>3 x '+ GETAL +'=' + GETAL);
-document.write('<br>4 x '+ GETAL +'=' + GETAL);
-document.write('<br>5 x '+ GETAL +'=' + GETAL);
-document.write('<br>6 x '+ GETAL +'=' + GETAL); 
-document.write('<br>7 x '+ GETAL +'=' + GETAL);
-document.write('<br>8 x '+ GETAL +'=' + GETAL);
-document.write('<br>9 x '+ GETAL +'=' + GETAL); 
-document.write('<br>10 x '+ GETAL +'=' + GETAL);
+document.write('<br>1 x '+ GETAL +'=' + 1*GETAL);
+document.write('<br>2 x '+ GETAL +'=' + 2*GETAL);
+document.write('<br>3 x '+ GETAL +'=' + 3*GETAL);
+document.write('<br>4 x '+ GETAL +'=' + 4*GETAL);
+document.write('<br>5 x '+ GETAL +'=' + 5*GETAL);
+document.write('<br>6 x '+ GETAL +'=' + 6*GETAL); 
+document.write('<br>7 x '+ GETAL +'=' + 7*GETAL);
+document.write('<br>8 x '+ GETAL +'=' + 8*GETAL);
+document.write('<br>9 x '+ GETAL +'=' + 9*GETAL); 
+document.write('<br>10 x '+ GETAL +'=' + 10*GETAL);
 
 
 
